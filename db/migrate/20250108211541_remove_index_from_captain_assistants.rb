41fb8f654a8d8599a3dd14a22a6fee9f8b29590f
Devansh Sanghavi <devansh.sanghavi@somaiya.edu>
2025-05-27 12:13:05 -0700

    3 files reverted back again so changed those, and skipped git hook (husky)

--- db/migrate/20250108211541_remove_index_from_captain_assistants.rb
+++ db/migrate/20250108211541_remove_index_from_captain_assistants.rb
@@ -1,4 +1,4 @@
-class RemoveIndexFromCaptainAssistants < ActiveRecord::Migration[7.0]
+class RemoveIndexFromCaptainTopics < ActiveRecord::Migration[7.0]
   def change
     remove_index :captain_assistants, [:account_id, :name], if_exists: true
   end
--- db/migrate/20250116000103_add_status_to_captain_assistant_responses.rb
+++ db/migrate/20250116000103_add_status_to_captain_assistant_responses.rb
@@ -1,4 +1,4 @@
-class AddStatusToCaptainAssistantResponses < ActiveRecord::Migration[7.0]
+class AddStatusToCaptainTopicResponses < ActiveRecord::Migration[7.0]
   def change
     add_column :captain_assistant_responses, :status, :integer, default: 1, null: false
     add_index :captain_assistant_responses, :status
